84d2708df56fc032a605577740bea580a994f80c
bobaronoff <bobaronoffMD@aol.com>
2023-01-06 19:38:51 -0500

    First Commit

--- src/SimpleXGB.jl
+++ src/SimpleXGB.jl
@@ -1,5 +1,12 @@
 module SimpleXGB
 
-# Write your package code here.
+# Utility functions to create XGBoost models
+
+using DataFrames,Dates
+# using SimpleDFutils
+using XGBoost
+using Statistics, StatsBase, Random
+using Distributions , GLM , Loess
+using Plots
 
 end
